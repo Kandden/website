63d7ed875c33722e3dadd90ce78046b694762ea9
Kandden <mathias-vestergaard@hotmail.com>
2024-02-28 20:00:19 +0100

    second barebones

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function AlbumPicker() {
+  const [albums, setAlbums] = useState<string[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      artist: { value: string };
+    };
+    const artist = encodeURIComponent(target.artist.value);
+    const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
+    const response = await fetch(url);
+    const mbResult = (await response.json()) as {
+      releases: { title: string }[];
+    };
+    const { releases } = mbResult;
+    setAlbums(releases.map(({ title }) => title));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Artist name:
+        <input name="artist" />
+      </label>
+      <button type="submit">Search</button>
+      <p>Albums:</p>
+      <ol>
+        {albums.map((album) => (
+          <li>{album}</li>
+        ))}
+      </ol>
+    </form>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +53,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <AlbumPicker />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
